8cdb48534af14e8314942264038598596a607d51
GabrielAvelarbr <99gabrielavelar@gmail.com>
2025-05-04 21:51:23 -0300

    adição de comentários arquivo furia_cs_data.py

--- src/lambda_function/furia_cs_data.py
+++ src/lambda_function/furia_cs_data.py
@@ -2,9 +2,11 @@ import requests
 import os
 from datetime import datetime, timezone, timedelta
 
-token =  os.getenv('pandascore_api_token')
+# Obtém o token da variável de ambiente
+token = os.getenv('pandascore_api_token')
 
-def make_request(url): 
+# Função para fazer requisição GET com cabeçalho de autorização
+def make_request(url):
     try:
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(url, headers=headers)
@@ -13,6 +15,7 @@ def make_request(url):
     except requests.RequestException as e:
         return f"Erro na requisição: {e}"
 
+# Função para obter a lineup time da FURIA
 def get_furia_lineup(time=None):
     url = f'https://api.pandascore.co/teams?filter[slug]=furia'
     data = make_request(url)
@@ -24,6 +27,7 @@ def get_furia_lineup(time=None):
         return "Jogadores não encontrados."
     return "Time não encontrado."
 
+# Função para obter a última partida da FURIA
 def get_furia_last_game(time=None):
     url = f'https://api.pandascore.co/csgo/matches/past?filter[opponent_id]=furia'
     data = make_request(url)
@@ -37,6 +41,7 @@ def get_furia_last_game(time=None):
         )
     return "Nenhuma partida passada encontrada."
 
+# Função para obter a próxima partida da FURIA
 def get_furia_next_game(time=None):
     url = f'https://api.pandascore.co/csgo/matches/upcoming?filter[opponent_id]=furia'
     data = make_request(url)
@@ -48,12 +53,15 @@ def get_furia_next_game(time=None):
 
         if match_datetime:
             try:
+                # Converte a data e hora para UTC
                 dt_utc = datetime.strptime(match_datetime, "%Y-%m-%dT%H:%M:%SZ")
                 dt_utc = dt_utc.replace(tzinfo=timezone.utc)
 
+                # Converte para o fuso horário do Brasil (GMT-3)
                 brazil_tz = timezone(timedelta(hours=-3))
                 dt_brazil = dt_utc.astimezone(brazil_tz)
 
+                # Formata a data e hora para o padrão brasileiro
                 formatted_date = dt_brazil.strftime("%d/%m/%Y às %H:%M")
             except ValueError:
                 formatted_date = match_datetime
